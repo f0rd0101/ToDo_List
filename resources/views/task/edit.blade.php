38d814762e622e765d2d676222353d718b52b17a
Nick <melancholic8078@gmail.com>
2025-07-15 00:44:13 +0300

    added styles

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -5,12 +5,12 @@
   @method('PATCH')
   <div class="form-group">
     <label>Task Name</label>
-    <input  name="title" class="form-control" id="exampleInputEmail1"  placeholder="What task do you want to acomplish today?" value = "{{ $task->title }}">
+    <input  name="title" class="form-control" id="exampleInputEmail1"  placeholder="Enter task name" value = "{{ $task->title }}">
     
   </div>
   <div class="form-group">
     <label>Content</label>
-    <input name="content" class="form-control"  id="exampleInputPassword1" placeholder="Tell more about what do you want to do" value = "{{ $task->content}}">
+    <input name="content" class="form-control"  id="exampleInputPassword1" placeholder="Add details" value = "{{ $task->content}}">
   </div>
   <button type="submit" class="btn btn-primary">Update</button>
 </form>
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.main')
 @section('content')
+
+
 <div>
+<div class='div-form'>
+<form action="{{ route('tasks.store') }}" method="post">
+  @csrf
+  <div class="form-group">
+    <label>Task Name</label>
+    <input  name="title" class="form-control" id="exampleInputEmail1"  placeholder="Enter task title">
+    
+  </div>
+  <div class="form-group">
+    <label>Content</label>
+    <input name="content" class="form-control"  id="exampleInputPassword1" placeholder="Add details about the task">
+  </div>
+  <button type="submit" class="btn btn-success d-block mx-auto mt-2">Add</button>
+</form>
+</div>
+<div class = "data-div">
     <ul>
         @foreach($tasks as $task)
-         <li>
+         <li class = "item-li">
            <h2>{{ $task->title }}</h2>
-           <p>{{ $task->content }}</p>
+           <p class="content-p">{{ $task->content }}</p>
+           <div class="buttons-div">
            <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary">Edit</a>
            <form action="{{ route('tasks.delete', $task->id) }}" method = "post">
             @csrf
             @method('DELETE')
             <input type="submit" value = 'Delete' class = 'btn btn-danger'>
+            </div>
            </form>
         
            
@@ -18,19 +38,8 @@
          </li>
         @endforeach
     </ul>
+    </div>
 </div>
 
-<form action="{{ route('tasks.store') }}" method="post">
-  @csrf
-  <div class="form-group">
-    <label>Task Name</label>
-    <input  name="title" class="form-control" id="exampleInputEmail1"  placeholder="What task do you want to acomplish today?">
-    
-  </div>
-  <div class="form-group">
-    <label>Content</label>
-    <input name="content" class="form-control"  id="exampleInputPassword1" placeholder="Tell more about what do you want to do">
-  </div>
-  <button type="submit" class="btn btn-primary">Add</button>
-</form>
+
 @endsection
