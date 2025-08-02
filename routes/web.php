5a4dc6b80feb0d06e61dff4ccdd64e22b430c34f
Nick <melancholic8078@gmail.com>
2025-08-02 14:02:18 +0300

    added breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tasks');
 });
 
 
+// CRUD ROUTES
 Route::get('/tasks',[TaskController::class,'index'])->name('tasks.index');
 Route::post('/tasks',[TaskController::class,'store'])->name('tasks.store');
 Route::delete('/tasks/{task}',[TaskController::class, 'delete'])->name('tasks.delete');
 Route::get('/tasks/{task}/edit',[TaskController::class, 'edit'])->name('tasks.edit');
 Route::patch('/tasks/{task}',[TaskController::class, 'update'])->name('tasks.update');
+
+
+
+// AUTH ROUTES
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
