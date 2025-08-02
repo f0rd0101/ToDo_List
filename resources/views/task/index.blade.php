be8a217371c4c9caf1b28bc069eedf24f2e0234a
Nick <melancholic8078@gmail.com>
2025-08-03 02:28:12 +0300

    added logout

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -3,10 +3,18 @@
 
 
 <div>
+
+
 <div class='div-form'>
-<form action="{{ route('tasks.store') }}" method="post">
+
+<form action="{{ route('tasks.store') }}" method="post" style="padding-top:100px">
+  
   @csrf
   <div class="form-group">
+    
+      @auth
+  <h1 style="color:white; margin-bottom:15px">Hello, {{ Auth::user()->name }} 👋</h1>
+  @endauth
     <label>Task Name</label>
     <input  name="title" class="form-control" id="exampleInputEmail1"  placeholder="Enter task title">
     
@@ -76,7 +84,28 @@ onmouseout="this.style.backgroundColor='#ef4444'; this.style.boxShadow='0 2px 6p
          </li>
         @endforeach
     </ul>
+    
     </div>
+    <form method="POST" action="{{ route('logout') }}">
+  @csrf
+  <button type="submit" style="
+    background-color: #4bad4f;
+    border-radius: 6px;
+    padding: 8px 16px;
+    border: none;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+    box-shadow: 0 2px 6px rgba(75, 173, 79, 0.4);
+    margin:0 auto;
+    display: block;
+    margin-top:30px
+  "
+  onmouseover="this.style.backgroundColor='#3a8c3f'; this.style.boxShadow='0 4px 12px rgba(58, 140, 63, 0.6)';"
+  onmouseout="this.style.backgroundColor='#4bad4f'; this.style.boxShadow='0 2px 6px rgba(75, 173, 79, 0.4)';"
+  >Logout</button>
+</form>
 </div>
 
 
